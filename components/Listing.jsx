f8f2fa92330e1bf83262c5fa07109ba63761d92c
Scott <richardsprins@gmail.com>
2020-07-21 14:20:42 -0400

    styling some components

--- components/Listing.jsx
+++ components/Listing.jsx
@@ -18,6 +18,33 @@ const Listing = (props) => {
         }
 
         .story-title {
+          font-size: 1rem;
+          font-weight: 400;
+          margin: 0;
+          margin-bottom: 0.5em;
+        }
+
+        .story-title a {
+          color: #333;
+          text-decoration: none;
+        }
+
+        .story-title a:hover, .story-details a:hover {
+          text-decoration: underline
+        }
+
+        .story-details {
+          font-size: 03.rem;
+          font-weight: bold
+        }
+
+        .story-details span {
+          margin-right: 1em; 
+        }
+
+        .story-details a {
+          color: #6600ff;
+          text-decoration: none;
           
         }
       `}</style>
